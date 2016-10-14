d71263e58515d9a5da372e3cc82a5b5f95419b25
r1ruest <rafael.ruest@insel.ch>
2016-10-14 16:23:54 +0200

    I wrote function makeCacheMatrix and CacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL               # assign the cache to m
+        set <- function(y) {    # this function assignes y to x
+                                # and the cache m to NULL in the parent enviroment
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x     # assignes the getter with x form the parent enviroment
+        set_inverse_matrix <- function(inverse) m <<- inverse # assignes
+        get_inverse_matrix <- function()        # defines the getter for the inverse Matrix
+        list(set = set, get = get    #makes a list with the function we set and name them
+             set_inverse_matrix = set_inverse_matrix,
+             get_inverse_matrix = get_inverse_matrix)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse matrix of the matrix "x" from the cache. If the is no value in the cache it calcualtes the inverese matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$get_inverse_matrix()     # assign the inverse matrix to m
+        if(!is.null(m)) {               # if the m is not NULL, prints massage "getting cache data"
+                message("getting cached data")
+                return(m)               # and returns m
+        }
+        data <- x$get()                 # else assign the getter from above function to data
+        m <- solve(data, ...)           # and recomputes the inverse matrix of "x"
+        x$set_inverse_matrix(m)         # sets m in the cache
+        m                               # prints  m
 }
+
